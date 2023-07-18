bbb780db6cbc34264748a0cf8257ca24de13ef9c
Luiz Bernardo <luiz.bernardo.ext@ipiranga.ipiranga>
2023-07-18 14:29:39 -0300

    teste

--- run.py
+++ run.py
@@ -8,3 +8,20 @@ def hello_word():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+# syntax=docker/dockerfile:1
+
+# FROM python:3.8-slim-buster
+
+# WORKDIR /app
+# ENV FLASK_APP run.py
+
+# COPY requirements.txt requirements.txt
+# RUN pip install -r requirements.txt
+
+# COPY . .
+
+# CMD [ "python3", "-m", "flask", "run", "--host=0.0.0.0" , "--port=5000"]
+
